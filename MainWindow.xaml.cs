3a8b40677083c88fb717e1419f0b6e279001e4d5
TheSolid-KANG <tortoise8947@gmail.com>
2022-12-10 14:54:30 +0900

    Caleb WPF

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Engine._02.KMP;
+using Engine._05.CStackTracer;
+using Engine._06.CINIMgr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Caleb
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+      Initialize();
+    }
+
+    public void Initialize()
+    {
+    }
+
+  }
+}
